eaec46d440488a32498abb946c4c810703a41c4e
michel <laurentmichelst@gmail.com>
2017-12-29 14:26:52 +0100

    implement menu in MainActivity.java

--- app/src/main/java/com/example/michel/mynews/MainActivity.java
+++ app/src/main/java/com/example/michel/mynews/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //configuring Toolbar
+        this.configureToolbar();
         // Configure tablayout
         this.configureViewPagerAndTabs();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //2 - Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    private void configureToolbar() {
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 
     private void configureViewPagerAndTabs(){
