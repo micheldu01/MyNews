1c76b22576f053e0845222a557bccc9f38b3d500
michel <laurentmichelst@gmail.com>
2017-12-31 10:38:19 +0100

    create AboutActivity java and xml

---
+++ app/src/main/java/com/example/michel/mynews/AboutActivity.java
@@ -0,0 +1,27 @@
+package com.example.michel.mynews;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        this.configureToolbar();
+    }
+    private void configureToolbar(){
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Set the toolbar
+        setSupportActionBar(toolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
