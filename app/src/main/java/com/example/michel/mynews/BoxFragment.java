1d53a285009a91883d5ac64e8a3f9c1b23175cd6
michel <laurentmichelst@gmail.com>
2018-01-01 11:18:40 +0100

    create BoxFragment java and xml

---
+++ app/src/main/java/com/example/michel/mynews/BoxFragment.java
@@ -0,0 +1,29 @@
+package com.example.michel.mynews;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BoxFragment extends Fragment {
+
+
+    public BoxFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_box, container, false);
+    }
+
+}
