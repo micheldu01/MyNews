1ddae462eca4f242771074f5fbdd48405fe295a3
michel <laurentmichelst@gmail.com>
2018-01-01 11:13:58 +0100

    create EditTextQueryFragment fragment java and xml

---
+++ app/src/main/java/com/example/michel/mynews/EditTextQueryTermFragment.java
@@ -0,0 +1,29 @@
+package com.example.michel.mynews;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditTextQueryTermFragment extends Fragment {
+
+
+    public EditTextQueryTermFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_text_query_term, container, false);
+    }
+
+}
