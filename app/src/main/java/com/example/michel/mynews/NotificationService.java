295cc5c749f927ffa7243d67a8a73c085b9f442a
michel <laurentmichelst@gmail.com>
2018-01-03 11:04:09 +0100

    add onStartCommand method inside AlarmReceiver

---
+++ app/src/main/java/com/example/michel/mynews/NotificationService.java
@@ -0,0 +1,46 @@
+package com.example.michel.mynews;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by michel on 03/01/2018.
+ */
+
+public class NotificationService extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        NotificationManager notify_manager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent_main_activity = new Intent(this.getApplicationContext(), MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,
+                intent_main_activity, 0);
+
+        Notification notification_poupup = new Notification.Builder(this)
+                .setContentTitle("le message est bien passé")
+                .setContentText("ajout du text")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+
+        notify_manager.notify(0,notification_poupup);
+
+
+        return START_NOT_STICKY;
+}
