e7aa9d30fe7316db5e1ca9dcfef812a48d3e85d0
michel <laurentmichelst@gmail.com>
2018-01-03 11:01:44 +0100

    create AlarmReceiver

---
+++ app/src/main/java/com/example/michel/mynews/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.example.michel.mynews;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by michel on 03/01/2018.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
