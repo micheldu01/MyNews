f4ef8d01811f110f0fac7fae448e250563e27139
michel <laurentmichelst@gmail.com>
2018-01-04 11:42:30 +0100

    implement select date in SearchActivity

--- app/src/main/java/com/example/michel/mynews/SearchActivity.java
+++ app/src/main/java/com/example/michel/mynews/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.michel.mynews;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SearchActivity extends AppCompatActivity
     implements BtnSearchFragment.OnButtonClickedListener,
@@ -27,6 +32,11 @@ public class SearchActivity extends AppCompatActivity
     private CheckBox[] arrayBox;
     private int[] arrayIdBox;
     private int numberarray = 0;
+    // Date
+    private TextView begin_date;
+    private TextView end_date;
+    private Calendar mCurrentDate;
+    private int day, month, year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +45,26 @@ public class SearchActivity extends AppCompatActivity
 
         this.configureToolbar();
 
+        //date
+        begin_date = (TextView) findViewById(R.id.show_date_begin);
+        end_date = (TextView) findViewById(R.id.show_date_end);
+        //CurrentDate
+        mCurrentDate = Calendar.getInstance();
+        mCurrentDate = Calendar.getInstance();
+        day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+        month = mCurrentDate.get(Calendar.MONTH);
+        year = mCurrentDate.get(Calendar.YEAR);
+        month = month +1;
+        begin_date.setText(day+"/"+month+"/"+year);
+
         //EditText
         editText = (EditText)findViewById(R.id.search_query_term);
+
         //CheckBox
         arrayBox = new CheckBox[] {arts, business, entrepreneurs, politics, travel, sport};
         arrayIdBox = new int[] {R.id.checkbox_art,R.id.checkbox_business,R.id.checkbox_entrepreneurs,
                 R.id.checkbox_politics,R.id.checkbox_travel,R.id.checkbox_sport};
+
         // implement CheckBox
         while(numberarray <6){
             arrayBox[numberarray] = (CheckBox)findViewById(arrayIdBox[numberarray]);
@@ -64,8 +88,6 @@ public class SearchActivity extends AppCompatActivity
     public void onButonClicked(View view) {
         // receive the editText
         String et = editText.getText().toString();
-
-        Log.i("mycoursviewpager", "Button clicked = ");
         Toast.makeText(SearchActivity.this," Button clicked ", Toast.LENGTH_LONG).show();
         if(arrayBox[0].isChecked()){
             Toast.makeText(SearchActivity.this," Arts ", Toast.LENGTH_LONG).show();
@@ -75,13 +97,27 @@ public class SearchActivity extends AppCompatActivity
 
     @Override
     public void onButonClickedDateLeft(View view) {
-        Toast.makeText(SearchActivity.this," Button left ", Toast.LENGTH_LONG).show();
-
+        DatePickerDialog datePickerDialog = new DatePickerDialog
+                (SearchActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int yeat, int month, int day) {
+                        begin_date.setText(day+"/"+month+"/"+year);
+                        month = month +1;
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
 
     @Override
     public void onButonClickedDateRight(View view) {
-        Toast.makeText(SearchActivity.this," Button right ", Toast.LENGTH_LONG).show();
-
+        DatePickerDialog datePickerDialog = new DatePickerDialog
+                (SearchActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int yeat, int month, int day) {
+                        end_date.setText(day+"/"+month+"/"+year);
+                        month = month +1;
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
 }
