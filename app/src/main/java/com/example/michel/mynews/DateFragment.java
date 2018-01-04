e16878511d61b12311815c6af9fc87be82f00f35
michel <laurentmichelst@gmail.com>
2018-01-04 11:22:35 +0100

    implement btn_date_left btn_date_right in DateFragment

--- app/src/main/java/com/example/michel/mynews/DateFragment.java
+++ app/src/main/java/com/example/michel/mynews/DateFragment.java
@@ -1,6 +1,7 @@
 package com.example.michel.mynews;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,8 +12,21 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DateFragment extends Fragment {
+public class DateFragment extends Fragment implements View.OnClickListener {
 
+    // declare callback
+    private OnButtonClickedListener mCallback;
+    private OnButtonClickedListener2 mCallback2;
+
+    // declare callback
+    public interface OnButtonClickedListener {
+        public void onButonClickedDateLeft(View view);
+    }
+
+    // declare callback
+    public interface OnButtonClickedListener2 {
+        public void onButonClickedDateRight(View view);
+    }
 
     public DateFragment() {
         // Required empty public constructor
@@ -23,7 +37,48 @@ public class DateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date, container, false);
+        View result = inflater.inflate(R.layout.fragment_date, container, false);
+        result.findViewById(R.id.btn_date_left).setOnClickListener(this);
+        // Inflate second image button
+        result.findViewById(R.id.btn_date_right).setOnClickListener(this);
+        return result;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Call the method that creating callback after being attached to parent activity
+        this.createCallbackToParentActivity();
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Here is handled the button click
+        // spread the click to the parent activity
+        // Choice between mCallback mCallback2
+        switch (view.getId()){
+            case R.id.btn_date_left:
+                mCallback.onButonClickedDateLeft(view);
+                break;
+            case R.id.btn_date_right:
+                mCallback2.onButonClickedDateRight(view);
+                break;
+        }
+    }
+
+    // create callback to parent activity
+    private void createCallbackToParentActivity(){
+        try {
+            // Parent activity will automatically subscribe to callback
+            mCallback = (DateFragment.OnButtonClickedListener) getActivity();
+        }catch (ClassCastException e){
+            throw new ClassCastException(e.toString()+" must implement OnButtonClickedListner");
+        }
+        try {
+            // Parent activity will automatically subscribe to callback
+            mCallback2 = (DateFragment.OnButtonClickedListener2) getActivity();
+        }catch (ClassCastException e){
+            throw new ClassCastException(e.toString()+" must implement OnButtonClickedListner");
+        }
+    }
 }
