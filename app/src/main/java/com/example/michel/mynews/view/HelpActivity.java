3656cd019f21e697dbc963e2e9ced9a51d2bae49
michel <laurentmichelst@gmail.com>
2018-01-15 14:39:55 +0100

    add text in activity_help

--- app/src/main/java/com/example/michel/mynews/view/HelpActivity.java
+++ app/src/main/java/com/example/michel/mynews/view/HelpActivity.java
@@ -4,15 +4,22 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.example.michel.mynews.R;
 
 public class HelpActivity extends AppCompatActivity {
 
+    //add TextView value
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        //implement TextView
+        textView = (TextView) findViewById(R.id.text_help);
 
         this.configureToolbar();
     }
