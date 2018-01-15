8f6826d2de6d1c7aac8471238d3b5c24f3e929d4
michel <laurentmichelst@gmail.com>
2018-01-15 12:21:12 +0100

    refactoring SharedPreferences in EditTextQuery

--- app/src/main/java/com/example/michel/mynews/controller/BoxFragment.java
+++ app/src/main/java/com/example/michel/mynews/controller/BoxFragment.java
@@ -27,5 +27,4 @@ public class BoxFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_box, container, false);
     }
-
 }
--- app/src/main/java/com/example/michel/mynews/controller/EditTextQueryTermFragment.java
+++ app/src/main/java/com/example/michel/mynews/controller/EditTextQueryTermFragment.java
@@ -18,9 +18,6 @@ import com.example.michel.mynews.R;
  */
 public class EditTextQueryTermFragment extends Fragment {
 
-    private EditText editText;
-    public static final String PREFS = "prefs";
-
     public EditTextQueryTermFragment() {
         // Required empty public constructor
     }
@@ -29,11 +26,6 @@ public class EditTextQueryTermFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_edit_text_query_term, container, false);
-        editText = (EditText)view.findViewById(R.id.search_query_term);
-        String st = editText.getText().toString();
-        SharedPreferences preferences = this.getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
-        preferences.edit().putString("values",st).commit();
-        return view;
+        return inflater.inflate(R.layout.fragment_edit_text_query_term, container, false);
     }
 }
