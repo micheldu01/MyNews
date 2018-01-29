0a7214ac6a2077292b44d0ae9fcb03f9d381ff3a
michel <laurentmichelst@gmail.com>
2018-01-29 14:45:24 +0100

    add permission internet in manifest

--- app/src/main/java/com/example/michel/mynews/controller/TopStoriesFragment.java
+++ app/src/main/java/com/example/michel/mynews/controller/TopStoriesFragment.java
@@ -22,5 +22,7 @@ public class TopStoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_top_stories, container, false);
     }
+
+
 }
 
