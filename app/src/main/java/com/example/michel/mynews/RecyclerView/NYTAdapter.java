7a50e1f2b53dfa00c51beb261d3cdce0b8fe4030
michel <laurentmichelst@gmail.com>
2018-02-05 10:19:34 +0100

    create adapter for recycler view

---
+++ app/src/main/java/com/example/michel/mynews/RecyclerView/NYTAdapter.java
@@ -0,0 +1,42 @@
+package com.example.michel.mynews.RecyclerView;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by michel on 03/02/2018.
+ */
+
+public class NYTAdapter extends RecyclerView.Adapter<NYTWiewHolder> {
+
+    List<MonObjet> monObjets;
+
+    public NYTAdapter(List<MonObjet> monObjets) {
+        this.monObjets = monObjets;
+    }
+
+    @Override
+    public NYTWiewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_main_item, parent, false);
+        return new NYTWiewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(NYTWiewHolder nytWiewHolder, int position) {
+        MonObjet monObjet = monObjets.get(position);
+        nytWiewHolder.bindtwo(monObjet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return monObjets.size();
+    }
+}
+
+
+
