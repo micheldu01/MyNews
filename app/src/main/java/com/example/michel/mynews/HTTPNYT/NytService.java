31a48643a9086a9cb5d807270309e49b045ca4a6
michel <laurentmichelst@gmail.com>
2018-02-05 10:15:13 +0100

    create NytService api NYT

---
+++ app/src/main/java/com/example/michel/mynews/HTTPNYT/NytService.java
@@ -0,0 +1,24 @@
+package com.example.michel.mynews.HTTPNYT;
+
+import io.reactivex.Observable;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+/**
+ * Created by michel on 03/02/2018.
+ */
+
+public interface NytService {
+
+    @GET("svc/topstories/v2/home.json?api-key=c69e095eadba4c708c5d4ffeb0699a41")
+    Observable<NYresult> getTopStories();
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.nytimes.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build();
+}
+
