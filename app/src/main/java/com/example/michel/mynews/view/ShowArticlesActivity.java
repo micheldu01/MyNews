b5e1532f24090a39feb88a6419731f7d17176ecd
michel <laurentmichelst@gmail.com>
2018-02-07 21:45:40 +0100

    create new activity ShowArticlesActivity

---
+++ app/src/main/java/com/example/michel/mynews/view/ShowArticlesActivity.java
@@ -0,0 +1,13 @@
+package com.example.michel.mynews.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowArticlesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_articles);
+    }
+}
