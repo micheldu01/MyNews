b7745437a10cb3ff64467962f254f840e726f548
michel <laurentmichelst@gmail.com>
2018-02-07 21:48:46 +0100

    add toolbar in ShowArticlesActivity

--- app/src/main/java/com/example/michel/mynews/view/ShowArticlesActivity.java
+++ app/src/main/java/com/example/michel/mynews/view/ShowArticlesActivity.java
@@ -1,7 +1,11 @@
 package com.example.michel.mynews.view;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.michel.mynews.R;
 
 public class ShowArticlesActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class ShowArticlesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_articles);
+
+
+        //Add toolbar
+        this.configureToolbar();
+
+    }
+
+    // method for create toolbar view
+    private void configureToolbar(){
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Set the toolbar
+        setSupportActionBar(toolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
