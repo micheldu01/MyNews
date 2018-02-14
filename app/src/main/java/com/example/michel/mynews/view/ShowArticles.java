9469be1cd2330fdb3b9385e217c92df98dc9a1bf
michel <laurentmichelst@gmail.com>
2018-02-14 15:00:33 +0100

    delete ShowArticles fragment

--- app/src/main/java/com/example/michel/mynews/view/ShowArticles.java
+++ app/src/main/java/com/example/michel/mynews/view/ShowArticles.java
@@ -11,5 +11,8 @@ public class ShowArticles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_articles);
+
+
+
     }
 }
