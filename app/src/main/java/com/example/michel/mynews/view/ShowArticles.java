1a9fb4792e46f43fd5e20c6bf9458e2340d5a3d1
michel <laurentmichelst@gmail.com>
2018-02-14 15:16:31 +0100

    add ActionBar in activity_show_articles xml

--- app/src/main/java/com/example/michel/mynews/view/ShowArticles.java
+++ app/src/main/java/com/example/michel/mynews/view/ShowArticles.java
@@ -1,9 +1,11 @@
 package com.example.michel.mynews.view;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
-import android.widget.Toolbar;
+
 
 import com.example.michel.mynews.R;
 
@@ -20,10 +22,13 @@ public class ShowArticles extends AppCompatActivity {
 
 
         webView = (WebView) findViewById(R.id.web_view_1);
+        toolbar = (Toolbar) findViewById(R.id.test_toolbar);
 
         webView.loadUrl("https://www.google.fr/");
 
-
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
     }
 }
