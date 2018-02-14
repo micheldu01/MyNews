9ba2e300581dc010268878bcfb9a13e551e39cdf
michel <laurentmichelst@gmail.com>
2018-02-14 15:13:06 +0100

    add WebView in activity_show_articles java

--- app/src/main/java/com/example/michel/mynews/view/ShowArticles.java
+++ app/src/main/java/com/example/michel/mynews/view/ShowArticles.java
@@ -2,15 +2,26 @@ package com.example.michel.mynews.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toolbar;
 
 import com.example.michel.mynews.R;
 
 public class ShowArticles extends AppCompatActivity {
 
+    private WebView webView;
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_articles);
+
+
+        webView = (WebView) findViewById(R.id.web_view_1);
+
+        webView.loadUrl("https://www.google.fr/");
 
 
 
