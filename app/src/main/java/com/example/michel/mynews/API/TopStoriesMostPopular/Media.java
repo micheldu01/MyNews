bef6eaff1ddf2582847c920aede8bd31c7b58534
michel <laurentmichelst@gmail.com>
2018-02-22 10:35:54 +0100

    add url in Media

--- app/src/main/java/com/example/michel/mynews/API/TopStoriesMostPopular/Media.java
+++ app/src/main/java/com/example/michel/mynews/API/TopStoriesMostPopular/Media.java
@@ -1,8 +1,23 @@
 package com.example.michel.mynews.API.TopStoriesMostPopular;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by michel on 22/02/2018.
  */
 
 public class Media {
+
+    @SerializedName("url")
+    @Expose
+    public String url;
+
+    public Media(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
