85dec84c5e8860adfef44a5d6f6a8b4f61e70f1a
michel <laurentmichelst@gmail.com>
2018-02-24 10:28:09 +0100

    ADD METHOD YES NO  IN NotificationService.java

--- app/src/main/java/com/example/michel/mynews/Notification/NotificationService.java
+++ app/src/main/java/com/example/michel/mynews/Notification/NotificationService.java
@@ -9,12 +9,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.example.michel.mynews.API.NytStreams;
+import com.example.michel.mynews.API.TopStoriesMostPopular.TopStoriesAPI;
 import com.example.michel.mynews.R;
 import com.example.michel.mynews.view.MainActivity;
 
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 /**
  * Created by michel on 03/01/2018.
@@ -70,7 +74,7 @@ public class NotificationService extends Service {
         // IF YES START THE NOTIFICATION
         //------------------------------
 
-        if(stt.equals("YES"){
+        if(stt.equals("YES")) {
 
             // create notification poupup
             Notification notification_poupup = new Notification.Builder(this)
@@ -87,11 +91,68 @@ public class NotificationService extends Service {
 
             // use notify
             notify_manager.notify(0, notification_poupup);
-
+            }
 
             return START_NOT_STICKY;
-        }
+    }
 
+    private void myHTTPAlarm() {
+
+        // 1.2 - Execute the stream subscribing to Observable defined inside GithubStream
+        this.disposable = NytStreams.streamTopStories()
+                .subscribeWith(new DisposableObserver<TopStoriesAPI>() {
+
+                    @Override
+                    public void onNext(TopStoriesAPI nYresult) {
+
+                        //--------------------------------
+                        //  I GET THE CURRENT TITRE
+                        //  I GET THE SHARED
+                        // AND I COMPARED WITH THE SHARED
+                        // IF THEY ARE DIFFERENT I SAVE A NEW SHARED
+                        // AND I MAKE A NOTIFICATION
+                        //-----------------------------------
+
+                        // I GET THE CURRENT TITRE
+                        str = nYresult.getResults().get(0).getTitle();
+                        // I GET THE SHARED
+                        String s = preferences.getString(TITRE,"");
+                        // I COMPARE THEM
+                        Log.e("mynews","récupération du shared &&&&&&&&&&&&" + s);
+
+                        if(!str.equals(s)){
+                            Log.e("mynews","test du if +++++++++++++++++++++++++" + s);
+                            preferences.edit().putString(TITRE, str).commit();
+                            preferences.edit().putString(YES_NO, "YES");
+                        }
+                        else{
+                            Log.e("mynews","test du if WWWWWWWWWWWWWWWWWWWWWWWWW" + s);
+                            preferences.edit().putString(TITRE, "je suis laurent").commit();
+
+                        }
+
+                        //----------------
+                        // TEST RECUP SHARED
+                        //-------------------
+
+                        Log.e("mynews", "ALARMMMMMMMMMM = " + s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("TAG", "On Error" + Log.getStackTraceString(e));
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e("TAG", "On Complete !!");
+                    }
+                });
+    }
+
+
+    private void disposeWhenDestroy(){
+        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
     }
 
     @Override
