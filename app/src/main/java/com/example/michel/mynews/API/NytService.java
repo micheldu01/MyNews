2fb7f3d74b0072f3cd473ec2c0f752bcb4694bba
michel <laurentmichelst@gmail.com>
2018-02-25 19:12:45 +0100

    create getBusiness in NYTService

--- app/src/main/java/com/example/michel/mynews/API/NytService.java
+++ app/src/main/java/com/example/michel/mynews/API/NytService.java
@@ -1,6 +1,7 @@
 package com.example.michel.mynews.API;
 
 import com.example.michel.mynews.API.MostPopular.MostPopular;
+import com.example.michel.mynews.API.SearchArticleAPI.SearchActicleAPI;
 import com.example.michel.mynews.API.TopStories.TopStoriesAPI;
 
 import io.reactivex.Observable;
@@ -8,6 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by michel on 03/02/2018.
@@ -20,6 +22,9 @@ public interface NytService {
 
     @GET("svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=c69e095eadba4c708c5d4ffeb0699a41")
     Observable<MostPopular> getMostPopular();
+
+    @GET("svc/search/v2/articlesearch.json?api-key=c69e095eadba4c708c5d4ffeb0699a41")
+    Observable<SearchActicleAPI> getBusiness(@Query("q") String term);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/")
