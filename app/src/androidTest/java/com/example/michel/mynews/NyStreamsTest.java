80e1ad7b75f96557b34356f4d39bdc988496884f
michel <laurentmichelst@gmail.com>
2018-02-25 18:50:43 +0100

    add test mostPopular

--- app/src/androidTest/java/com/example/michel/mynews/NyStreamsTest.java
+++ app/src/androidTest/java/com/example/michel/mynews/NyStreamsTest.java
@@ -37,11 +37,37 @@ public class NyStreamsTest {
         // 4 - Test if streamTopStories is empty
         TopStoriesAPI storiesAPI = testObserver.values().get(0);
         // 5 - Ask if getResults is different to null
-        assertThat("result NYT",  storiesAPI.getResults() !=  null);
+        assertThat("result NYT", storiesAPI.getResults() != null);
 
-        assertThat("result NYT",  storiesAPI.getResults().get(0).getTitle() !=  null);
+        assertThat("result NYT", storiesAPI.getResults().get(0).getTitle() != null);
 
-        assertThat("result NYT",  storiesAPI.getResults().get(0).getMultimedia().get(0).getUrl() !=  null);
+        assertThat("result NYT", storiesAPI.getResults().get(0).getMultimedia().get(0).getUrl() != null);
+
+    }
+
+    @Test
+    public void mostPopularTest() throws Exception {
+
+        // 1 - Get the stream
+        //     Recupération de la stream
+        io.reactivex.Observable<MostPopular> observableMostPopular =
+                NytStreams.streamMostPopular();
+
+        // - 2 Create a new TestObserver
+        //     Création d'un nouveau TestObserver
+        TestObserver<MostPopular> testObserver = new TestObserver<>();
+
+        // 3 - Launch observable
+        //     Lancement d'un observable
+        observableMostPopular.subscribeWith(testObserver)
+                .assertNoErrors() // 3.1 - Check if  no errors
+                .assertNoTimeout() // 3.2 - Check if no Timeout
+                .awaitTerminalEvent(); // - Await the stream terminated before continue
+
+        // 4 - Test if streamTopStories is empty
+        MostPopular mostPopular = testObserver.values().get(0);
+        // 5 - Ask if getResults is different to null
+        assertThat("result NYT",  mostPopular.getResults() !=  null);
 
     }
 }
