f4d693c49ddf0ee962db25f977a997d955eaf129
michel <laurentmichelst@gmail.com>
2018-02-25 19:08:17 +0100

    create Byline in API

---
+++ app/src/main/java/com/example/michel/mynews/API/SearchArticleAPI/Byline.java
@@ -0,0 +1,41 @@
+package com.example.michel.mynews.API.SearchArticleAPI;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by michel on 25/02/2018.
+ */
+
+public class Byline {
+
+    @SerializedName("original")
+    @Expose
+    public String original;
+    @SerializedName("person")
+    @Expose
+    public List<Person> person = null;
+    @SerializedName("organization")
+    @Expose
+    public String organization;
+
+    public Byline(String original, List<Person> person, String organization) {
+        this.original = original;
+        this.person = person;
+        this.organization = organization;
+    }
+
+    public String getOriginal() {
+        return original;
+    }
+
+    public List<Person> getPerson() {
+        return person;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+}
