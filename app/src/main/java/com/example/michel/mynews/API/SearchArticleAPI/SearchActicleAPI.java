a39753ddb8f32eed16b13891e90447acb66a2075
michel <laurentmichelst@gmail.com>
2018-02-25 19:04:04 +0100

    create SearchArticleAPI in API

--- app/src/main/java/com/example/michel/mynews/API/SearchArticleAPI/SearchActicleAPI.java
+++ app/src/main/java/com/example/michel/mynews/API/SearchArticleAPI/SearchActicleAPI.java
@@ -1,8 +1,40 @@
 package com.example.michel.mynews.API.SearchArticleAPI;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by michel on 06/02/2018.
  */
 
 public class SearchActicleAPI {
+
+
+    @SerializedName("status")
+    @Expose
+    public String status;
+    @SerializedName("copyright")
+    @Expose
+    public String copyright;
+    @SerializedName("response")
+    @Expose
+    public Response response;
+
+    public SearchActicleAPI(String status, String copyright, Response response) {
+        this.status = status;
+        this.copyright = copyright;
+        this.response = response;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public Response getResponse() {
+        return response;
+    }
 }
