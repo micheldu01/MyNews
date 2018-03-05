10cd82032f340ca23edbfe89878567e66461c322
michel <laurentmichelst@gmail.com>
2018-03-05 19:17:13 +0100

    add toolbar in ViewSearchArticles

--- app/src/main/java/com/example/michel/mynews/view/ViewSearchArticles.java
+++ app/src/main/java/com/example/michel/mynews/view/ViewSearchArticles.java
@@ -1,10 +1,41 @@
 package com.example.michel.mynews.view;
 
-/**
- * Created by michel on 05/03/2018.
- */
 
-public class ViewSearchArticles {
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 
+import com.example.michel.mynews.R;
+
+public class ViewSearchArticles extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_search_articles);
+
+
+        //show the toolbar
+        this.configureToolbar();
+
+
+    }
+
+
+    private void configureToolbar() {
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Set the toolbar
+        setSupportActionBar(toolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
 }
+
+
+
